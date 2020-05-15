40aa206962639fcd6beaf10ba998a330851dfccd
John Mendonca <joaosinho@gmail.com>
2020-05-15 17:24:04 -0600

    Try out Tailwind stacked layout on index page

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,22 @@ import Nav from '../components/Nav'
 export default () => (
   <div>
     <Nav />
+    <header className="bg-white shadow">
+      <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold leading-tight text-gray-900">
+          Next.js + Tailwind CSS
+        </h1>
+      </div>
+    </header>
+    <main>
+      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div className="px-4 py-6 sm:px-0">
+          <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+        </div>
+      </div>
+    </main>
     <div className="hero">
-      <h1 className="title">Next.js + Tailwind CSS</h1>
+      <h1 className="title"></h1>
     </div>
   </div>
 )
