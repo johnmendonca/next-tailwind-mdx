f55a8fb99b94873f4fe1b51e6c17f7a51e10e6bd
John Mendonca <joaosinho@gmail.com>
2020-05-26 11:41:18 -0600

    index.mdx and README

---
+++ README.md
@@ -0,0 +1,7 @@
+## next-tailwind-mdx
+
+ * Next.js 9.4
+ * TailwindCSS and TailwindUI
+ * MDX
+
+This uses example React components made from the free TailwindUI components available [here](https://tailwindui.com/preview).
--- components/MarketingLayout.js
+++ components/MarketingLayout.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import MarketingHeader from '../components/marketing/MarketingHeader'
+import { MDXProvider } from '@mdx-js/react'
+import MarketingHeader from './marketing/MarketingHeader'
 
 const MarketingLayout = ({children}) => {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
       <MarketingHeader />
-      {children}
+      <div className="px-4 py-6 sm:px-0">
+        {children}
+      </div>
     </div>
   )
 }
